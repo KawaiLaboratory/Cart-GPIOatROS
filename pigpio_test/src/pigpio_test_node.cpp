7133c2fd50cef1b7a10d23de67f52ea761d13d72
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-09-14 13:29:04 +0900

    cp

--- pigpio_test/src/pigpio_test_node.cpp
+++ pigpio_test/src/pigpio_test_node.cpp
@@ -7,8 +7,11 @@
 #define DOT(x, x_prev, dt) (((x)-(x_prev))/(dt))
 
 #define HALF 500000 //duty比50%
-#define PHASE_DIFF 2*M_PI //位相遅れ[rad]
-#define WALK_SPEED 1.7 //[m/s]
+#define PHASE_DIFF 1.5*M_PI //位相遅れ[rad]
+#define KPl 1 //will change
+#define KDl 1 //will change
+#define KPt 1 //will change
+#define KDt 1 //will change
 
 int pi;
 extern int pi;
@@ -23,8 +26,9 @@ static float b = 0.0036;  //モータ分解能[deg]
 static float safety = 0.5; //安全距離[m]
 
 float l = 0.0; //得られた最近点までの距離[m]
+float l_prev = 0.0;
 float theta = 0.0; //得られた最近点の角度[rad]
-float theta_prev = 0.0; //直前の角度[rad]
+float theta_prev = 0.0;
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
@@ -33,12 +37,11 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
   theta = 0.0;
 
   for(int i = 0; i < count; i++) {
-    float rad = fmodf(scan->angle_min + PHASE_DIFF + scan->angle_increment * i, 2*M_PI);
-    float degree = RAD2DEG(rad);
+    float degree = RAD2DEG(fmodf(scan->angle_min + PHASE_DIFF + scan->angle_increment * i, 2*M_PI));
     if(0.0 < degree && degree < 180.0){
-      if(l == 0.0 || l > scan->ranges[i]){
+      if(i == 0 || l > scan->ranges[i]){
         l = scan->ranges[i];
-        theta = rad;
+        theta = degree;
       }
     }
   }
@@ -53,14 +56,16 @@ int main(int argc, char **argv)
   ros::Time now;
   ros::Duration duration;
   ros::Subscriber sub;
+
   double dt = 0.0;
-  float x = 0.0;
-  float y = 0.0;
   bool lost = false;
-  double freq_l = 0.0;
-  double freq_r = 0.0;
-  double v = WALK_SPEED;
+  double u_l = 0.0;
+  double u_r = 0.0;
+  double v = 0.0;
+  double ohm = 0.0;
+  
   double d_theta = 0.0;
+  double d_l = 0.0;
 
   pi = pigpio_start("localhost","8888");
   set_mode(pi, pwmpin[0], PI_OUTPUT);
@@ -69,43 +74,58 @@ int main(int argc, char **argv)
   set_mode(pi, dirpin[1], PI_OUTPUT);
 
   while(ros::ok()){
-    sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
-
-    loop_rate.sleep();
-    ros::spinOnce();
-
     now = ros::Time::now();
     duration = now - prev;
     dt = duration.toSec();
-
-//    if(l <= safety){
-//      ROS_INFO("Too Close");
-//      hardware_PWM(pi, pwmpin[0], 0, 0);
-//      hardware_PWM(pi, pwmpin[1], 0, 0);
-//    }else if(!lost){
+    
+    //if(l <= safety){
+      //ROS_INFO("Too Close");
+      //hardware_PWM(pi, pwmpin[0], 0, 0);
+      //hardware_PWM(pi, pwmpin[1], 0, 0);
+    //}else if(!lost){
+         
       d_theta = DOT(theta, theta_prev, dt);
-      freq_l = 180.0 * (v-d*d_theta)/(M_PI*b*r);
-      freq_r = 180.0 * (v+d*d_theta)/(M_PI*b*r);
-
+      d_l     = DOT(l, l_prev, dt);
+      
+      v   = M_PI * b * r / 360 * ( u_r + u_l/d);
+      ohm = M_PI * b * r / 360 * ( u_r - u_l/d);
+      
+      u_r = ( KPl * ( l - 1 ) + KDl * ( d_l - v ) + KPt * ( theta - M_PI/2 ) + KDt * ( d_theta - ohm ))*1/2;
+      u_l = ( KPl * ( l - 1 ) + KDl * ( d_l - v ) - KPt * ( theta - M_PI/2 ) - KDt * ( d_theta - ohm ))*d/2;
+      
       ROS_INFO("dt:%lf", dt);
-      ROS_INFO("d_theta:%lf", d_theta);
-      ROS_INFO("freq_l:%lf", freq_l);
-      ROS_INFO("freq_r:%lf", freq_r);
-      ROS_INFO("freq_l:%d", (int)freq_l);
-      ROS_INFO("freq_r:%d", (int)freq_r);
-      //x = distance * std::cos(angle);
-      //y = distance * std::sin(angle);
-      //ROS_INFO("x:%f, y:%f", x, y);
-      //ROS_INFO("%f", freq);
-      //gpio_write(pi, dirpin[0], PI_HIGH);
-      //gpio_write(pi, dirpin[1], PI_LOW);
-      //hardware_PWM(pi, pwmpin[0], (int)freq, HALF);
-      //hardware_PWM(pi, pwmpin[1], (int)freq, HALF);
-//    }else{
-//      ROS_INFO("MADA");
-//    }
-    prev = now;
+      ROS_INFO("u_l:%lf", u_l);
+      ROS_INFO("u_r:%lf", u_r);
+      ROS_INFO("v:%lf", v);
+      ROS_INFO("ohm:%lf", ohm);
+      
+      if(u_r < 0){
+        gpio_write(pi, dirpin[0], PI_LOW);
+      }else{
+        gpio_write(pi, dirpin[0], PI_HIGH);
+      }
+      
+      if(u_l < 0){
+        gpio_write(pi, dirpin[1], PI_HIGH);
+      }else{
+        gpio_write(pi, dirpin[1], PI_LOW);
+      }
+      
+      hardware_PWM(pi, pwmpin[0], (int)u_r, HALF);
+      hardware_PWM(pi, pwmpin[1], (int)u_l, HALF);
+    //}else{
+    //  ROS_INFO("mada");
+    //}
+    
     theta_prev = theta;
+    l_prev = l;
+    
+    sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
+
+    prev = now;
+    loop_rate.sleep();
+    ros::spinOnce();
+    
   }
 
   // 出力信号の停止
