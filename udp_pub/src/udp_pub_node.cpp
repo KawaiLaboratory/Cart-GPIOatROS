843e472c669414b1a416097e11b8ea8daa460638
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-10-10 11:16:18 +0900

    totyuu

--- udp_pub/src/udp_pub_node.cpp
+++ udp_pub/src/udp_pub_node.cpp
@@ -1,23 +1,31 @@
+//for LRF
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+//for UDP
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char **argv)
 {
- int sock;
- struct sockaddr_in addr;
+  ros::init(argc, argv, "udp_pub");
+  ros::NodeHandle n;
+  ros::Subscriber sub;
+  ros::Rate loop_rate(10);
+  int sock;
+  struct sockaddr_in addr;
 
- sock = socket(AF_INET, SOCK_DGRAM, 0);
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
 
- addr.sin_family = AF_INET;
- addr.sin_port = htons(12345);
- addr.sin_addr.s_addr = inet_addr("169.254.170.171");
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(12345);
+  addr.sin_addr.s_addr = inet_addr("169.254.170.171");
 
- sendto(sock, "HELLO", 5, 0, (struct sockaddr *)&addr, sizeof(addr));
+  sendto(sock, "HELLO", 5, 0, (struct sockaddr *)&addr, sizeof(addr));
 
- close(sock);
+  close(sock);
 
  return 0;
 }
