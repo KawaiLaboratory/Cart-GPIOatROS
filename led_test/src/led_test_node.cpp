d81154e92fd58ae7801cdf5442a66387f7876f3d
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-11-07 13:02:27 +0900

    re

--- led_test/src/led_test_node.cpp
+++ led_test/src/led_test_node.cpp
@@ -24,13 +24,13 @@ int main(int argc, char **argv){
   set_mode(pi, SETUP_LED, PI_OUTPUT);
   set_mode(pi, DRIVING_LED, PI_OUTPUT);
 
-  gpioSetPullUpDown(pi, SHUTDOWN_PIN, PI_PUD_UP);
-  gpioSetPullUpDown(pi, CLUTCH_PIN, PI_PUD_UP);
-  gpioSetPullUpDown(pi, START_PIN, PI_PUD_UP);
-  gpioSetPullUpDown(pi, SETUP_LED, PI_PUD_DOWN);
-  gpioSetPullUpDown(pi, DRIVING_LED, PI_PUD_DOWN);
+  set_pull_up_down(pi, SHUTDOWN_PIN, PI_PUD_UP);
+  set_pull_up_down(pi, CLUTCH_PIN, PI_PUD_UP);
+  set_pull_up_down(pi, START_PIN, PI_PUD_UP);
+  set_pull_up_down(pi, SETUP_LED, PI_PUD_DOWN);
+  set_pull_up_down(pi, DRIVING_LED, PI_PUD_DOWN);
 
-  printf("GPIO%d is level %d", SHUTDOWN_PIN, gpioRead(pi, SHUTDOWN_PIN));
-  printf("GPIO%d is level %d", CLUTCH_PIN, gpioRead(pi, CLUTCH_PIN));
-  printf("GPIO%d is level %d", START_PIN, gpioRead(pi, START_PIN));
+  printf("GPIO%d is level %d", SHUTDOWN_PIN, gpio_read(pi, SHUTDOWN_PIN);
+  printf("GPIO%d is level %d", CLUTCH_PIN, gpio_read(pi, CLUTCH_PIN);
+  printf("GPIO%d is level %d", START_PIN, gpio_read(pi, START_PIN);
 }
