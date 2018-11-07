9e565a329ed48d6c8f344161e6108fcffcec1b58
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-11-07 12:44:59 +0900

    check

--- led_test/src/led_test_node.cpp
+++ led_test/src/led_test_node.cpp
@@ -2,6 +2,12 @@
 #include "pigpiod_if2.h"
 #include "math.h"
 
+#define SHUTDOWN_PIN 29
+#define CLUTCH_PIN   31
+#define START_PIN    32 
+#define SETUP_LED    38
+#define DRIVING_LED  40
+
 int pi;
 extern int pi;
 
@@ -11,9 +17,20 @@ int main(int argc, char **argv){
   ros::Publisher pub;
   ros::Rate loop_rate(20);
 
-  static int flash_pin[2]={38, 40};
-
   pi = pigpio_start("localhost", "8888");
-  set_mode(pi, flash_pin[0], PI_INPUT);
-  set_mode(pi, flash_pin[1], PI_INPUT);
+  set_mode(pi, SHUTDOWN_PIN, PI_INPUT);
+  set_mode(pi, CLUTCH_PIN, PI_INPUT);
+  set_mode(pi, START_PIN, PI_INPUT);
+  set_mode(pi, SETUP_LED, PI_OUTPUT);
+  set_mode(pi, DRIVING_LED, PI_OUTPUT);
+
+  gpioSetPullUpDown(SHUTDOWN_PIN, PI_PUD_UP);
+  gpioSetPullUpDown(CLUTCH_PIN, PI_PUD_UP);
+  gpioSetPullUpDown(START_PIN, PI_PUD_UP);
+  gpioSetPullUpDown(SETUP_LED, PI_PUD_DOWN);
+  gpioSetPullUpDown(DRIVING_LED, PI_PUD_DOWN);
+
+  printf("GPIO%d is level %d", SHUTDOWN_PIN, gpioRead(SHUTDOWN_PIN));
+  printf("GPIO%d is level %d", CLUTCH_PIN, gpioRead(CLUTCH_PIN));
+  printf("GPIO%d is level %d", START_PIN, gpioRead(START_PIN));
 }
