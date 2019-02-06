905befbbeb24b80e8ed9024be840bbe04edba9ac
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2019-02-06 13:02:38 +0900

    comment

--- led_test/src/led_test_node.cpp
+++ led_test/src/led_test_node.cpp
@@ -3,62 +3,73 @@
 #include "math.h"
 #include "std_msgs/Bool.h"
 
+/* GPIOピン番号の宣言 */
 #define SHUTDOWN_PIN 17
 #define CLUTCH_PIN   27
 #define START_PIN    22
 #define SETUP_LED    26
 #define DRIVING_LED  12
 
+/* PIGPIOとの接続用 */
 int pi;
 extern int pi;
 
 int main(int argc, char **argv){
+  /*--- ROS用宣言 ---*/
   ros::init(argc, argv, "led_test");
   ros::NodeHandle n;
   ros::Publisher pub;
   ros::Rate loop_rate(20);
 
+  /*--- 運転フラグ送信用宣言 ---*/
   pub = n.advertise<std_msgs::Bool>("driving_flag", 1000);
   std_msgs::Bool flag;
 
+  /*--- PIGPIO制御 ---*/
   pi = pigpio_start("localhost", "8888");
-  set_mode(pi, SHUTDOWN_PIN, PI_INPUT);
-  set_mode(pi, CLUTCH_PIN, PI_INPUT);
-  set_mode(pi, START_PIN, PI_INPUT);
-  set_mode(pi, SETUP_LED, PI_OUTPUT);
-  set_mode(pi, DRIVING_LED, PI_OUTPUT);
+  set_mode(pi, SHUTDOWN_PIN, PI_INPUT);   // 緊急停止スイッチ読み取り用
+  set_mode(pi, CLUTCH_PIN, PI_INPUT);     // 電磁クラッチスイッチ読み取り用
+  set_mode(pi, START_PIN, PI_INPUT);      // スタートスイッチ読み取り用
+  set_mode(pi, SETUP_LED, PI_OUTPUT);     // 緑色LED出力用
+  set_mode(pi, DRIVING_LED, PI_OUTPUT);   // 赤色LED出力用
 
-  bool ClutchFlg   = false;
-  bool ShutdownFlg = false;
-  bool InitFlg     = false;
-  bool DrivingFlg  = false;
+  /*--- 処理用フラグ宣言  ---*/
+  bool ClutchFlg   = false;               // クラッチのONOFF
+  bool ShutdownFlg = false;               // 緊急停止スイッチのONOFF
+  bool InitFlg     = false;               // 走行準備完了フラグ
+  bool DrivingFlg  = false;               // 運転中フラグ
 
-  int clutch_status;
-  int shutdown_status;
+  /*--- 処理簡略化用変数宣言 ---*/
+  int clutch_status　= 0;
+  int shutdown_status = 0;
 
   while(ros::ok()){
+    /*--- クラッチと緊急停止スイッチの状態読み取り ---*/
     clutch_status   = gpio_read(pi, CLUTCH_PIN);
     shutdown_status = gpio_read(pi, SHUTDOWN_PIN);
 
-    ShutdownFlg = (shutdown_status == PI_LOW)? true : false;
-    ClutchFlg   = (clutch_status   == PI_LOW)? true : false;
-    InitFlg     = (ClutchFlg && ShutdownFlg) ? true : false;
+    /*--- フラグ状態の切り替え処理 ---*/
+    ShutdownFlg = (shutdown_status == PI_LOW)? true : false;  // 緊急停止スイッチのフラグ切り替え
+    ClutchFlg   = (clutch_status   == PI_LOW)? true : false;  // クラッチのフラグ切り替え
+    InitFlg     = (ClutchFlg && ShutdownFlg) ? true : false;  // 走行が可能か切り替え
 
-    gpio_write(pi, SETUP_LED, InitFlg);
+    gpio_write(pi, SETUP_LED, InitFlg);                       // 緑色LEDの点灯消灯
 
+    /*--- 運転状態の切り替え ---*/
     if(InitFlg){
-      if(wait_for_edge(pi, START_PIN, FALLING_EDGE, 0.05))
-        DrivingFlg = !DrivingFlg;
+      if(wait_for_edge(pi, START_PIN, FALLING_EDGE, 0.05))    // スタートスイッチの割り込み待機
+        DrivingFlg = !DrivingFlg;                             // 運転開始フラグの反転
     }else{
-      DrivingFlg = false;
+      DrivingFlg = false;                                     // 運転開始フラグをへし折る
     }
 
-    flag.data = DrivingFlg;
-    pub.publish(flag);
+    flag.data = DrivingFlg;                                   // 運転開始フラグの配信準備
+    pub.publish(flag);                                        // 運転開始フラグの配信準備
 
-    gpio_write(pi,DRIVING_LED, DrivingFlg);
+    gpio_write(pi,DRIVING_LED, DrivingFlg);                   // 赤色LEDの点灯消灯
 
-    ros::spinOnce();
+    /*--- ループ ---*/
+    ros::spinOnce();  
     loop_rate.sleep();
   }
 }
--- pigpio_test/src/pigpio_test_node.cpp
+++ pigpio_test/src/pigpio_test_node.cpp
@@ -6,7 +6,7 @@
 #include "fstream"
 #include "ctime"
 
-/* --- 時間微分 --- */
+/* --- 時間微分関数 --- */
 #define TIMEDIFF(now, prev, dt) (((now)-(prev))/(dt))
 
 /* --- 各種定数 --- */
@@ -15,7 +15,7 @@
 #define r 0.15/2            // タイヤ半径[m]
 #define d 0.66/2            // 期待幅/2[m]
 #define KP 0.4              // 比例ゲイン
-#define KI 0.0             // 積分ゲイン #基本的に0
+#define KI 0.0              // 積分ゲイン(基本的に0)
 #define KD 0.04             // 微分ゲイン
 
 /* --- 各種グローバル変数 --- */
@@ -26,20 +26,23 @@ static int dirpin[2] = {20, 21};  // 回転方向用ピン
 double x_p = 0.0;                 // 目標点のx座標
 double y_p = 0.0;                 // 目標点のy座標
 bool lost = true;                 // 対象の認識T/F
-bool driving_flg = false;
+bool driving_flg = false;         // 運転開始フラグ
 
+/*--- 目標点コールバック関数 ---*/
 void PointCallback(const std_msgs::Float32MultiArray::ConstPtr& status){
   lost = (status->data[0] != 0.0);
   if(!lost){
-    x_p = -1*status->data[2];
-    y_p = status->data[1];
+    x_p = -1*status->data[2];   // LRF設置の角度補正
+    y_p = status->data[1];      // 同上
   }
 }
 
+/*--- 運転準備完了フラグコールバック関数 ---*/
 void FlagCallback(const std_msgs::Bool::ConstPtr& flag){
-  driving_flg = flag->data;
+  driving_flg = flag->data;     // 運転開始かどうか購読
 }
 
+/*--- GPIOの開始及び終了用関数 ---*/
 void changeGPIO(int status){
   for (int i = 0; i < 2; i++){
     set_mode(pi, pwmpin[i], status);
@@ -47,6 +50,7 @@ void changeGPIO(int status){
   }
 }
 
+/*--- PWM信号の停止用関数 ---*/
 void stopPulse(){
   for (int i = 0; i < 2; i++){
     hardware_PWM(pi, pwmpin[i], 0, 0);
@@ -54,6 +58,7 @@ void stopPulse(){
   }
 }
 
+/*--- ROSのループ用関数 ---*/
 double scaning(){
   ros::Rate loop_rate(20);
   ros::Time prev = ros::Time::now();
@@ -65,6 +70,7 @@ double scaning(){
   return (now - prev).toSec();
 }
 
+
 int main(int argc, char **argv){
   ros::init(argc, argv, "pigpio_test");
   ros::NodeHandle n;
@@ -73,32 +79,32 @@ int main(int argc, char **argv){
 
   pi = pigpio_start("localhost","8888");
 
-// カート用変数
-  double u_l   = 0.0; double u_r   = 0.0; // 左右モータへの入力周波数
-  double ul4in = 0.0; double ur4in = 0.0;
+  /*--- カート用変数 ---*/
+  double u_l   = 0.0; double u_r   = 0.0; // 左右モータへの計算上の入力周波数
+  double ul4in = 0.0; double ur4in = 0.0; // 左右モータへの実際の入力周波数(abs(u_hoge))
   double v     = 0.0; double ohm   = 0.0; // 極座標での速度,角速度
-  double x_c   = 0.0; double y_c   = 0.0;
-  double phi   = M_PI/2;
+  double x_c   = 0.0; double y_c   = 0.0; // カートの位置
+  double phi   = M_PI/2;                  // カートの姿勢
   double dx_c  = 0.0; double dy_c  = 0.0; // xy座標での速度
-// 対象点用変数
-  double x_pprev = 0.0; double y_pprev = 0.0;
-// 制御用変数
+  /*--- 対象点用変数 ---*/
+  double x_pprev = 0.0; double y_pprev = 0.0; // 目標点の前位置
+  /*--- 制御用変数 ---*/
   double e_x     = 0.0; double e_y     = 0.0; // 位置偏差
-  double de_x    = 0.0; double de_y    = 0.0;
-  double u_x     = 0.0; double u_y     = 0.0; // 偏差合計(一時変数)
-  double tmpIx   = 0.0; double tmpIy   = 0.0; // 積分項
-  double dt      = 0.0; double time    = 0.0; // 制御周期
-  double alpha   = 0.0; double l       = 1.0; // 角度誤差, 距離誤差
+  double u_x     = 0.0; double u_y     = 0.0; // PD制御による入力
+  double tmpIx   = 0.0; double tmpIy   = 0.0; // 積分項用一時変数
+  double dt      = 0.0; double time    = 0.0; // 制御周期, 待機時間
+  double alpha   = 0.0; double l       = 1.0; // 角度誤差, 参照距離
   bool setupFlg = false;                      // 点の初期設定フラグ
 
-  p_sub = n.subscribe("/status", 1000, PointCallback);
-  f_sub = n.subscribe("/driving_flag", 5, FlagCallback);
+  /*--- メッセージ購読 ---*/ 
+  p_sub = n.subscribe("/status", 1000, PointCallback);    // 目標点座標及び検出TFの購読
+  f_sub = n.subscribe("/driving_flag", 5, FlagCallback);  // 運転開始フラグの購読
 
-  // csv吐き出し用準備(実使用時は消す)
-  std::ofstream fs(std::to_string(std::time(nullptr))+".csv");
-  double time4csv = 0.0;
-  // ここまで
+  /*--- csv吐き出し用準備(実使用時は消す) ---*/
+  std::ofstream fs(std::to_string(std::time(nullptr))+".csv");  // CSVファイル生成
+  double time4csv = 0.0;                                        // 記録用時間
 
+  /*--- 人物の初期位置の検出 ---*/
   while(!setupFlg){
     dt = scaning();
 
@@ -106,84 +112,87 @@ int main(int argc, char **argv){
     setupFlg = (x_p != 0.0 && y_p != 0.0)? true : false;
   }
 
-  // csv書き込み(実使用時は消す)
+  /*--- csv書き込み(実使用時は消す) ---*/
   fs << "time,xp,xc,yp,yc,phi,ur,ul,lost,kp,ki,kd" << std::endl;
   fs << time4csv << "," << x_p << "," << x_c << "," << y_p << "," << y_c << "," << phi << ",";
   fs << u_r << "," << u_l << ","<< lost  << "," << KP << "," << KI << "," << KD << std::endl;
-  // ここまで
 
   while(ros::ok()){
     if(driving_flg){
       time = 0.0;
-      /*====入力部分====*/
-      changeGPIO(PI_OUTPUT);
+      /*=== 入力部分 ===*/
+      changeGPIO(PI_OUTPUT);  // モータのGPIOピンを出力状態に変更 
 
+      /*--- 入力用に符号変更処理 ---*/
       if(u_r < 0){
-        gpio_write(pi, dirpin[0], PI_LOW);
-        ur4in = std::fabs(u_r);
+        gpio_write(pi, dirpin[0], PI_LOW);  // タイヤの回転方向指定
+        ur4in = std::fabs(u_r);             // 入力周波数指定
       }else{
-        gpio_write(pi, dirpin[0], PI_HIGH);
-        ur4in = u_r;
+        gpio_write(pi, dirpin[0], PI_HIGH); // タイヤの回転方向指定
+        ur4in = u_r;                        // 入力周波数指定
       }
-
       if(u_l < 0){
-        gpio_write(pi, dirpin[1], PI_HIGH);
-        ul4in = std::fabs(u_l);
+        gpio_write(pi, dirpin[1], PI_HIGH); // タイヤの回転方向指定
+        ul4in = std::fabs(u_l);             // 入力周波数指定
       }else{
-        gpio_write(pi, dirpin[1], PI_LOW);
-        ul4in = u_l;
+        gpio_write(pi, dirpin[1], PI_LOW);  // タイヤの回転方向指定
+        ul4in = u_l;                        // 入力周波数指定
       }
 
+      /*--- PWM生成 ---*/
       hardware_PWM(pi, pwmpin[0], (int)ul4in, HALF);
       hardware_PWM(pi, pwmpin[1], (int)ur4in, HALF);
 
+      /*--- カートの速度・加速度計算 ---*/
       v   = R*r/2*(u_r + u_l);
       ohm = R*r/(2*d)*(u_r - u_l);
 
+      /*--- カートのxy座標での速度計算 ---*/ 
       dx_c = v*std::cos(phi);
       dy_c = v*std::sin(phi);
 
-      if(lost){
+      /*--- 検出の可否で位置姿勢の場合分け ---*/
+      if(lost){           // 非検出の場合
         x_c += dx_c * dt;
         y_c += dy_c * dt;
         phi += ohm * dt;
-      }else{
+      }else{              // 検出できている場合
         x_c = 0.0;
         y_c = 0.0;
         phi = M_PI/2;
       }
 
       /*====比較部分====*/
-      dt = scaning();
+      dt = scaning();   // 制御周期
 
-      e_x = (std::abs(x_p-x_c)<0.5)? 0.0 : x_p - x_c;
-      e_y = (0 < y_p-y_c && y_p-y_c < 1)? 0.0 : y_p - y_c;
+      e_x = (std::abs(x_p-x_c)<0.5)?      0.0 : x_p - x_c;  // x座標の偏差
+      e_y = (0 < y_p-y_c && y_p-y_c < 1)? 0.0 : y_p - y_c;  // y座標の偏差
+      alpha = std::atan2(y_p - y_c, x_p - x_c)-phi;　        // 角度偏差
 
       /*====フィードバック部分====*/
-      tmpIx += e_x*dt;
-      tmpIy += e_y*dt;
+      tmpIx += e_x*dt;  // xの積分項
+      tmpIy += e_y*dt;  // yの積分項
 
-      alpha = std::atan2(y_p - y_c, x_p - x_c)-phi;
+      u_x = KP*e_x - KD*dx_c; // PD制御による入力
+      u_y = KP*e_y - KD*dy_c; // PD制御による入力
 
-      u_x = KP*e_x - KD*dx_c;
-      u_y = KP*e_y - KD*dy_c;
+      u_r = 1/(R*r)*std::sqrt(u_x*u_x+u_y*u_y)*(1+2*d*std::sin(alpha)/l);   // PurePursuitアルゴリズムを用いたモータへの入力周波数
+      u_l = 1/(R*r)*std::sqrt(u_x*u_x+u_y*u_y)*(1-2*d*std::sin(alpha)/l);   // PurePursuitアルゴリズムを用いたモータへの入力周波数
 
-      u_r = 1/(R*r)*std::sqrt(u_x*u_x+u_y*u_y)*(1+2*d*std::sin(alpha)/l);
-      u_l = 1/(R*r)*std::sqrt(u_x*u_x+u_y*u_y)*(1-2*d*std::sin(alpha)/l);
+      x_pprev = x_p;  // 前位置の更新
+      y_pprev = y_p;  // 前位置の更新
 
-      x_pprev = x_p;
-      y_pprev = y_p;
-
-      // csv書き込み用(実使用時は消す)
+      /*--- csv書き込み用(実使用時は消す) ---*/
       time4csv += dt;
       fs << time4csv << "," << x_p << "," << x_c << "," << y_p << "," << y_c << "," << phi << ",";
       fs << u_r << "," << u_l << ","<< lost << std::endl;
-      // ここまで
     }else{
-      stopPulse();
-      changeGPIO(PI_INPUT);
+      stopPulse();          // PWMの停止
+      changeGPIO(PI_INPUT); // GPIOを入力状態に変更
 
-      time += scaning();
+      time += scaning();    // 待機時間の加算
+      
+      /*--- 停止処理 ---*/
       if (time > 30.0){
         fs.close();
         ROS_INFO("system is shutdown!");
@@ -192,11 +201,11 @@ int main(int argc, char **argv){
     }
   }
 
-  // PINOUT -> PININ
+  /*--- 信号出力の停止 ---*/
   stopPulse();
   changeGPIO(PI_INPUT);
 
-  // 終了
+  /*--- PIGPIOの終了 ---*/
   pigpio_stop(pi);
 
   return 0;
