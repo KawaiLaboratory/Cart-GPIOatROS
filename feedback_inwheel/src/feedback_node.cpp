da7c86baea41cbfbf35099ba887152ea17098bb0
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-01-31 13:22:24 +0900

    use callback

--- feedback_inwheel/src/feedback_node.cpp
+++ feedback_inwheel/src/feedback_node.cpp
@@ -11,6 +11,12 @@ using namespace std;
 
 int pi;
 extern int pi;
+double dt_r = 0.0;
+double dt_l = 0.0;
+ros::Time t_r;
+ros::Time t_r_prev;
+ros::Time t_l;
+ros::Time t_l_prev;
 
 class Cartbot{
   private:
@@ -37,27 +43,24 @@ class Serial{
     const int pin_dir[2] = {23, 24};
     const int pin_hs[2]  = {7 , 16}; // {7, 16, 20, 21, 25}
     const int FREQ       = 10000;
-    const int FORWARD    = 1;
-    const int REVERSE    = -1;
     int u_r_in = 0;
     int u_l_in = 0;
-    double v_r = 0.0;
-    double v_l = 0.0;
-    int r_dir  = 0;
-    int l_dir  = 0;
   public:
     Serial(){
       pi = pigpio_start("localhost","8888");
+
       if(pi < 0){
         printf("GPIO ERROR");
       }
       for (int i = 0; i < 2; i++){
         set_mode(pi, pin_pwm[i], PI_OUTPUT);
         set_mode(pi, pin_dir[i], PI_OUTPUT);
+        set_mode(pi, pin_hs[i],  PI_INPUT);
       }
-      for (int i = 0; i < 2; i++){
-        set_mode(pi, pin_hs[i], PI_INPUT);
-      }
+      t_r_prev = ros::Time::now();
+      t_l_prev = ros::Time::now();
+      callback(pi, pin_hs[0], FALLING_EDGE, &Serial::enc_r);
+      callback(pi, pin_hs[1], FALLING_EDGE, &Serial::enc_l);
     };
     ~Serial(){
       for (int i = 0; i < 2; i++){
@@ -72,80 +75,32 @@ class Serial{
       if(u_r < 0){
         gpio_write(pi, pin_dir[0], PI_LOW);  // タイヤの回転方向指定
         u_r_in = abs(u_r);                  // 入力周波数指定
-        r_dir = REVERSE;
       }else{
         gpio_write(pi, pin_dir[0], PI_HIGH); // タイヤの回転方向指定
         u_r_in = u_r;                       // 入力周波数指定
-        r_dir = FORWARD;
       }
       if(u_l < 0){
-        gpio_write(pi, pin_dir[1], PI_HIGH); // タイヤの回転方向指定
+        gpio_write(pi, pin_dir[1], PI_LOW); // タイヤの回転方向指定
         u_l_in = abs(u_l);                  // 入力周波数指定
-        l_dir = REVERSE;
       }else{
-        gpio_write(pi, pin_dir[1], PI_LOW);  // タイヤの回転方向指定
+        gpio_write(pi, pin_dir[1], PI_HIGH);  // タイヤの回転方向指定
         u_l_in = u_l;                       // 入力周波数指定
-        l_dir = FORWARD;
       }
 
       hardware_PWM(pi, pin_pwm[0], FREQ, u_r_in);
       hardware_PWM(pi, pin_pwm[1], FREQ, u_l_in);
     };
-    void enc_thread_r(const float r){
-      ros::Time r_prev = ros::Time::now();
-      ros::Time r_now;
-      ros::Rate rate(100);
-      double    r_dt = 0.0;
-      int       r_level_prev = gpio_read(pi, pin_hs[0]);
-      int       r_level_now  = r_level_prev;
-
-      while(ros::ok()){
-        r_level_now = gpio_read(pi, pin_hs[0]);
-        if(r_level_now != r_level_prev && r_level_now == PI_LOW){
-          r_now  = ros::Time::now();
-          r_dt   = (r_now-r_prev).toSec();
-
-          v_r = (2*M_PI*r)/(15*r_dt);
-
-          r_level_prev = r_level_now;
-          r_prev = r_now;
-        }
-        rate.sleep();
-      }
+    static void enc_r(int pi, unsigned int gpio, unsigned int level, uint32_t tick){
+      t_r      = ros::Time::now();
+      dt_r     = (t_r-t_r_prev).toSec();
+      t_r_prev = t_r;
+      cout << "R" << endl;
     };
-    void enc_thread_l(const float r){
-      ros::Time l_prev = ros::Time::now();
-      ros::Time l_now;
-      ros::Rate rate(100);
-      double    l_dt = 0.0;
-      int       l_level_prev = gpio_read(pi, pin_hs[1]);
-      int       l_level_now  = l_level_prev;
-
-      while(ros::ok()){
-        l_level_now = gpio_read(pi, pin_hs[1]);
-        if(l_level_now != l_level_prev && l_level_now == PI_LOW){
-          l_now = ros::Time::now();
-          l_dt  = (l_now-l_prev).toSec();
-
-          v_l = (2*M_PI*r)/(15*l_dt);
-
-          l_level_prev = l_level_now;
-          l_prev = l_now;
-        }
-        rate.sleep();
-      }
-    };
-    void thread_start(const float r){
-      thread thread_r( &Serial::enc_thread_r, this, r );
-      thread thread_l( &Serial::enc_thread_l, this, r );
-
-      thread_r.detach();
-      thread_l.detach();
-    };
-    tuple<double, double> get_enc(){
-      double v_r_dir = r_dir * v_r;
-      double v_l_dir = l_dir * v_l;
-      return {v_r_dir, v_l_dir};
+    static void enc_l(int pi, unsigned int gpio, unsigned int level, uint32_t tick){
+      t_l      = ros::Time::now();
+      dt_l     = (t_l-t_l_prev).toSec();
+      t_l_prev = t_l;
+      cout << "L" << endl;
     };
 };
 
@@ -163,6 +118,7 @@ class Controller{
     const float  T = 0.61;   // トレッド
     /* saturation */
     const int   MAX_DUTY = 1000000;
+    const int   MIN_DUTY = 1000;
     const float MAX_V    = 1.5;
     const float MAX_OM   = M_PI;
     /* debugmode */
@@ -197,9 +153,8 @@ class Controller{
     ros::Time current = start;
   public:
     Controller(){
-      ser.thread_start(r);
       if(debug_flg){
-        fs.open("~/catkin_ws/src/feedback_inwheel/csv/"+to_string(std::time(nullptr))+".csv");
+        fs.open(to_string(std::time(nullptr))+".csv");
         fs << "t,x,y,th,v_enc,om_enc,u_v,u_om,u_r,u_l,v_r,v_l,x_e,y_e,th_e,Kx,Ky,Kth,v_d,om_d" << endl;
         output_statuses(true);
       }
@@ -239,28 +194,27 @@ class Controller{
         u_r = -MAX_DUTY;
       }else if(u_r > MAX_DUTY){
         u_r = MAX_DUTY;
+      }else if(abs(u_r) < MIN_DUTY){
+        u_r = 0;
       }
       if(u_l < -MAX_DUTY){
         u_l = -MAX_DUTY;
       }else if(u_l > MAX_DUTY){
         u_l = MAX_DUTY;
+      }else if(abs(u_l) < MIN_DUTY){
+        u_l = 0;
       }
 
-      ser.input(u_r, u_l);
+//      ser.input(u_r, u_l);
       if(debug_flg){
         output_statuses();
       }
     };
     void get_u(){
-      auto u = ser.get_enc();
-      v_r = get<0>(u);
-      v_l = get<1>(u);
-
-      v_enc  = (v_r + v_l)/2;
-      om_enc = (v_r - v_l)/T;
-      u_v  = v_enc;
-      u_om = om_enc;
-    };
+      v_r = (2*M_PI*r)/(dt_r*15);
+      v_l = (2*M_PI*r)/(dt_l*15);
+      cout << v_r << "," << v_l << endl;
+    }
     void output_statuses(bool setup_flg = false){
       current = ros::Time::now();
       fs << (current-start).toSec() << ",";
